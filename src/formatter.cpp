f9d36075a3f3ff4c1c20a6aeaa35a0856b4691ea
Edgar Kalkowski <edgar.kalkowski@uni-kassel.de>
2013-04-09 12:40:38 +0200

    Remove legacy code.

--- src/formatter.cpp
+++ src/formatter.cpp
@@ -9,27 +9,6 @@ using std::endl;
 
 namespace dbshell {
 
-vector<uint> size(const vector<string>& header, const vector<vector<string>>& rows) {
-  vector<uint> sizes;
-
-  for (uint i = 0; i < header.size(); i++) {
-    sizes.push_back(header[i].size());
-  }
-
-  for (vector<string> row : rows) {
-
-    for (uint i = 0; i < header.size(); i++) {
-      const uint size = row[i].size();
-
-      if (size > sizes[i]) {
-        sizes[i] = size;
-      }
-    }
-  }
-
-  return sizes;
-}
-
 inline wstring fill(long ms) {
 
   if (ms < 10) {
@@ -98,105 +77,6 @@ wstring format_duration(long ms) {
   buffer << m << ":";
   if (s < 10) buffer << "0";
   buffer << s;
-  return buffer.str();
-}
-
-string format_header(const vector<string>& header, const vector<uint>& sizes, stringstream& buffer) {
-
-  for (uint i = 0; i < header.size(); i++) {
-    buffer << "│ ";
-
-    for (uint k = 0; k < (sizes[i] - header[i].length()) / 2; k++) {
-      buffer << " ";
-    }
-
-    buffer << header[i] << " ";
-
-    for (uint k = 0; k < (sizes[i] - header[i].length() + 1) / 2; k++) {
-      buffer << " ";
-    }
-  }
-
-  buffer << "│" << endl;
-  return buffer.str();
-}
-
-string format_rows(const vector<vector<string>>& rows, const vector<uint>& sizes, stringstream& buffer) {
-
-  for (vector<string> row : rows) {
-
-    for (uint i = 0; i < row.size(); i++) {
-      buffer << "│";
-
-      for (uint k = 0; k < 1 + (sizes[i] - row[i].length() + 1) / 2; k++) {
-        buffer << " ";
-      }
-
-      buffer << row[i];
-
-      for (uint k = 0; k < 1 + (sizes[i] - row[i].length()) / 2; k++) {
-        buffer << " ";
-      }
-    }
-
-    buffer << "│" << endl;
-  }
-
-  return buffer.str();
-}
-
-string format(const vector<string>& header, const vector<vector<string>>& rows) {
-  vector<uint> sizes = size(header, rows);
-  stringstream buffer;
-  buffer << "┌";
-
-  for (uint i = 0; i < sizes.size(); i++) {
-
-    for (uint k = 0; k < sizes[i] + 2; k++) {
-      buffer << "─";
-    }
-
-    if (i < sizes.size() - 1) {
-      buffer << "┬";
-    } else {
-      buffer << "┐" << endl;
-    }
-  }
-
-  format_header(header, sizes, buffer);
-
-  buffer << "├";
-
-  for (uint i = 0; i < sizes.size(); i++) {
-
-    for (uint k = 0; k < sizes[i] + 2; k++) {
-      buffer << "─";
-    }
-
-    if (i < sizes.size() - 1) {
-      buffer << "┼";
-    } else {
-      buffer << "┤" << endl;
-    }
-  }
-
-  format_rows(rows, sizes, buffer);
-
-  buffer << "└";
-
-  for (uint i = 0; i < sizes.size(); i++) {
-
-    for (uint k = 0; k < sizes[i] + 2; k++) {
-      buffer << "─";
-    }
-
-    if (i < sizes.size() - 1) {
-      buffer << "┴";
-    } else {
-      buffer << "┘";
-    }
-  }
-
   return buffer.str();
 }
 
--- src/formatter.hpp
+++ src/formatter.hpp
@@ -4,14 +4,6 @@
 
 namespace dbshell {
 
-std::vector<uint> size(const std::vector<std::string>& header, const std::vector<std::vector<std::string>>& rows);
-
-std::string format_header(const std::vector<std::string>& header, const std::vector<uint>& sizes, std::stringstream& buffer);
-
-std::string format_rows(const std::vector<std::vector<std::string>>& rows, const std::vector<uint>& sizes, std::stringstream& buffer);
-
-std::string format(const std::vector<std::string>& header, const std::vector<std::vector<std::string>>& rows);
-
 std::wstring format_duration(const long ms);
 
 } /* namespace dbshell */
